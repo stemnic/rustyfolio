d0adcc31a6b78fbd07745d7797ba984f1e88942b
Ole Sivert Aarhaug <ole.sivert@gmail.com>
2025-04-20 11:21:59 +0200

    Added csv export for FIFO calc

--- src/tax.rs
+++ src/tax.rs
@@ -1,10 +1,21 @@
+use std::io::Write;
+
 use crate::portfolio::{Action, Portfolio, Stock};
 use chrono::Datelike;
 use log::{debug, info};
 pub struct TaxCalculatorService {}
-
+static OUTPUT_FILE: &str = "output.csv";
 impl TaxCalculatorService {
+    fn overwrite_to_output_file(content: &str) -> Result<(), std::io::Error> {
+        let filename = format!("{}/output/{}", env!("CARGO_MANIFEST_DIR"), OUTPUT_FILE);
+        let mut file = std::fs::File::create(filename)?;
+        file.write_all(content.as_bytes())?;
+        info!("Writing output to {}", OUTPUT_FILE);
+        Ok(())
+    }
+
     pub fn fifo_calculation(portfolio: &Portfolio) {
+        let mut output_string = String::new();
         for ticker in portfolio.stocks.iter() {
             info!("FIFO {}:", ticker.ticker);
             let mut sell: Vec<Stock> = vec![];
@@ -21,6 +32,9 @@ impl TaxCalculatorService {
             let mut last_year = 0;
             let mut sum_shares_sold = 0.0;
             let mut sum_gains_loss = 0.0;
+            output_string = format!(
+                "Date,UnitsSold,GainOrLoss,BuyPrice,SellPrice,Diff,Profit,Currency,SellMetadata"
+            );
             for sell_stock in sell.iter_mut() {
                 year = sell_stock.date.year();
                 if last_year != year {
@@ -46,13 +60,28 @@ impl TaxCalculatorService {
                     }
                     let g_lstring = if gains >= 0.0 { "gain" } else { "loss" };
                     info!(
-                        "{} {} \t {} {} \t {} {}",
+                        "{} {} \t {} {} {} {} \t {} {}",
                         sell_stock.date,
                         unit_sold,
                         g_lstring,
+                        buy_pos.price,
+                        sell_stock.price,
                         gains,
                         gains * unit_sold,
                         buy_pos.currency
+                    );
+                    output_string = format!(
+                        "{}\n{},{},{},{},{},{},{},{},{}",
+                        output_string,
+                        sell_stock.date,
+                        unit_sold,
+                        g_lstring,
+                        buy_pos.price,
+                        sell_stock.price,
+                        gains,
+                        gains * unit_sold,
+                        buy_pos.currency,
+                        sell_stock.metadata
                     );
                     sum_gains_loss = sum_gains_loss + (gains * unit_sold);
                     sum_shares_sold = sum_shares_sold + unit_sold;
@@ -63,7 +92,8 @@ impl TaxCalculatorService {
                 last_year = year;
             }
             info!("{}: {} {}", year, sum_shares_sold, sum_gains_loss);
-            debug!("{:?}", buy.last())
+            debug!("{:?}", buy.last());
+            Self::overwrite_to_output_file(output_string.as_str());
         }
     }
 }
